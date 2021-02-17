c4afefb2309a11f8b93663975eaf2a188a0d5618
pawancr7 <pavankumarhoshur@gmail.com>
2021-02-17 13:08:52 +0530

    diff

---
+++ page/tester.js
@@ -0,0 +1,23 @@
+const number=5;
+const sum = number+2;
+ //name='siddiq';
+console.log(sum);
+
+//shows in console like this
+//name='siddiq'
+//TypeError: Assignment to constant variable
+
+let hero = 'hulk';
+//let hero = 'superman';
+console.log(hero);
+
+// let hero = 'superman';
+//syntaxError:Identifier 'hero' has already been declared
+
+let heros = 'loki';
+heros = 'thor';
+console.log(heros);//output will come updated value thor
+
+var heroine = 'natasha';
+var heroine = 'wonderwoman';
+console.log(heroine);//gives updated output wonderwoman
