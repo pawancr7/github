67f2ea71fa73705c7041194405e18184390961aa
pawancr7 <pavankumarhoshur@gmail.com>
2021-03-02 23:35:40 +0530

    k

---
+++ page/array/arrayfilter.js
@@ -0,0 +1,13 @@
+let products=[{name:'iphone12',price:50000,color:'red'},
+              {name:'redmi',price:60000,color:'white'},
+              {name:'samsung',price:30000,color:'blue'},
+              {name:'honor',price:18000,color:'black'},];
+         
+               
+         
+             for (x of products)
+             {
+          let x=products.filter((product)=> {product.color==='white' || products.color== "red")}
+                          return product };
+             
+             console.log(products);
--- page/array/arraymethods.js
+++ page/array/arraymethods.js
@@ -1,13 +1,7 @@
  // array push,pop,shift,unshift
- let movie = ["RRR","kgf","kd"];
- console.log(movie.shift());
- console.log(movie.pop); 
- console.log(movie[1]);
-/*
-x = {
-    test:'val',
-    test1:'val1'
-}
-
-console.log([x])
-*/
+ let size= ["rrr","kgf","ddlj"];
+ // nconsole.log(movie.shift());
+/*  console.log(movie.pop);  */
+ /* console.log(movie[1]); */
+ size.pop();
+ console.log(size);
